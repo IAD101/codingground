2e42f7cc289e7483275257d5aa2272c050b09be3
Israel Abreu <IAD101@users.noreply.github.com>
2017-02-27 16:34:37 -0400

    Created by Coding Ground Client

---
+++ New Project-20170227/CUSTOMERS.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+/*create table CUSTOMERS1(ID INT NOT NULL, NAME VARCHAR (20) NOT NULL, AGE INT NOT NULL, ADDRESS CHAR (25), SALARY DECIMAL (18,2), PRIMARY KEY(ID));
+insert into CUSTOMERS1 values(1,'Ramesh',32,'Ahmedabad',2000.00);                                                
+insert into CUSTOMERS1 values(2,'Khilan',25,'Delhi',1500.00);                                                    
+insert into CUSTOMERS1 values(3,'Kaushik',23,'Kota',2000.00);                                                    
+insert into CUSTOMERS1 values(4,'Chaitali',25,'Mumbai',6500.00);                                                 
+insert into CUSTOMERS1 values(5,'Hardik',27,'Bhopal',8500.00);                                                                          
+insert into CUSTOMERS1 values(6,'Komal',22,'MP',4500.00);
+insert into CUSTOMERS1 values(7,'Muffy',24,'Indore',10000.00);
+
+DROP TABLE CUSTOMERS;*/
+
+SELECT * FROM CUSTOMERS;
